315d5349024fd175dab8dce7024184907e5a48e3
현민 <143337620+Ggulbogpig@users.noreply.github.com>
2026-07-15 17:45:22 +0900

    Initial Unity project upload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    //스테이지 이동이랑 점수 관리
+    public int totalPoint;
+    public int stagePoint;
+    public int stageIndex;
+    public int health;
+    public PlayerMove player;
+    public GameObject[] Stages;
+    public GameObject UIRestartBtn;
+
+    public Image[] UIHealth;
+    public Text UIPoint;
+    public Text UIStage;
+
+    // Start is called before the first frame update
+    public void NextStage()
+    {
+        //Change Stage
+        if (stageIndex < Stages.Length-1)
+        {
+            Stages[stageIndex].SetActive(false);
+            stageIndex++;
+            Stages[stageIndex].SetActive(true);
+            PlayerReposition();
+
+            UIStage.text = "STAGE" + (stageIndex + 1);
+        }
+        else
+        {
+            Time.timeScale = 0;
+            Debug.Log("게임 클리어!");
+           
+            Text btnText = UIRestartBtn.GetComponentInChildren<Text>();
+            btnText.text = "Clear!";
+            UIRestartBtn.SetActive(true);
+        }
+
+
+        //Calculate Point
+        totalPoint += stagePoint;
+        stagePoint = 0;
+    }
+
+    public void HealthDown()
+    {
+        if (health > 1)
+        {
+            health--;
+            UIHealth[health].color = new Color(1, 0, 0, 0.4f);
+        }
+        else
+        {
+            UIHealth[0].color = new Color(1, 0, 0, 0.4f);
+            player.OnDie();
+
+            Debug.Log("죽었습니다!");
+            
+            UIRestartBtn.SetActive(true);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (health > 1)
+            {
+                //Player Reposition
+                PlayerReposition();
+            }
+
+        }
+        //Health Down
+        HealthDown();
+    }
+
+    void PlayerReposition()
+    {
+        player.transform.position = new Vector3(0, 0, -1);
+        player.VelocityZero();
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        UIPoint.text = (totalPoint + stagePoint).ToString();
+    }
+}
